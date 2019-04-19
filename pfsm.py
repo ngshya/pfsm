ab6979ec95e2f472d864028ec2ba1ba9ee2ba8bc
ngshya <ngshya@gmail.com>
2019-04-19 05:07:55 +0200

    First code base.

---
+++ pfsm.py
@@ -0,0 +1,50 @@
+# https://bergvca.github.io/2017/10/14/super-fast-string-matching.html
+
+import re
+
+def ngrams(string, n=3):
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+    ngrams = zip(*[string[i:] for i in range(n)])
+    return [''.join(ngram) for ngram in ngrams]
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidfM(array_names):
+    vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+    tf_idf_matrix = vectorizer.fit_transform(array_names)
+    return tf_idf_matrix
+
+
+
+import numpy as np
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct
+
+def awesome_cossim_top(A, B, ntop, lower_bound=0):
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+ 
+    idx_dtype = np.int32
+ 
+    nnz_max = M*ntop
+ 
+    indptr = np.zeros(M+1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+
+    return csr_matrix((data,indices,indptr),shape=(M,N))
