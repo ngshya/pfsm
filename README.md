2ec9cdb2b21da0535d57eedf1693a4d92eed7cea
ngshya <ngshya@gmail.com>
2019-04-20 03:22:10 +0200

    Python package creation

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Python Fast Strings Matching
+
+This package provides a fast way to perform strig matching tasks. 
+
+## Installing pfsm
+
+```bash
+pip install pfsm
+```
+
+## Example
+
+```python
+from pfsm import pfsm
+pfsm(["Company name 1", "Company name 2", "Another company name"])
+
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'Python Fast Strings Matching',
+  packages = ['pfsm'],
+  version = '0.1',
+  license='GNU General Public License v3.0',
+  description = 'Python Fast Strings Matching',
+  author = 'ngshya',
+  author_email = 'ngshya@gmail.com',
+  url = 'https://github.com/ngshya/pfsm',
+  download_url = 'https://github.com/ngshya/pfsm/archive/v0.1.tar.gz',
+  keywords = ['strings matching', 'strings distance', 'cosine similarity', 'ngrams', 'tf-idf', 'fast', 'python'],
+  install_requires=[
+      'unidecode',
+      'scikit-learn',
+      'numpy', 
+      'scipy',
+      'sparse_dot_topn',
+      'pandas'
+      ],
+  package_data={
+        'notebooks': ['notebooks/*'],
+        'data': ['data/*']
+    }
+  classifiers=[
+    'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
+    'Intended Audience :: Developers',
+    'Topic :: Text Processing',
+    'License :: OSI Approved ::  GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.6'
+  ],
+)
